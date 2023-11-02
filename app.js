28a5ab3f1e401ee8247a8a183c6c07c6ebd6eb04
ext.21m2016@etu.emse.fr <ext.21m2016@etu.emse.fr>
2023-11-02 16:03:50 +0100

    Implement logic and add mole image

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let molePosition 
+let currentTime = 60
 
 function randomSquare() {
     squares.forEach(square => {
@@ -14,11 +16,34 @@ function randomSquare() {
     let randomSquare = squares[Math.floor(Math.random() * 9)]
     
     randomSquare.classList.add('mole')
+    molePosition = randomSquare.id
 }
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == molePosition){
+            result++
+            score.innerHTML = `Score: ${result}`
+            molePosition = null
+        }
+    })
+})
 
 function moveMole(){
     let timerId = null
     timerId = setInterval(randomSquare, 500)
 }
 
-moveMole()
+moveMole() 
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0) {
+        clearInterval(countDownTimerId)
+        alert(`GAME OVER! Your final score is: ${result}`)
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
